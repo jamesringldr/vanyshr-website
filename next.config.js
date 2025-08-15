e8d369285f4313a48739d4fe8fd5255cfc908aa5
jamesringldr <james@ringldr.com>
2025-08-15 01:25:31 -0500

    Update config for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  // Update this with your actual repository name
+  // Updated with actual repository name
   assetPrefix: process.env.NODE_ENV === 'production' ? '/vanyshr-website' : '',
   basePath: process.env.NODE_ENV === 'production' ? '/vanyshr-website' : ''
 };
